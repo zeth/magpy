2ed89a03436eff974c52d695063168b4c4689c45
zeth <theology@gmail.com>
2013-01-29 14:00:33 +0000

    Warn on broken url files.

--- magpy/server/urlloader.py
+++ magpy/server/urlloader.py
@@ -24,7 +24,8 @@ class URLLoader(object):
             try:
                 url_module = importlib.import_module(url_path)
             except:
-                pass
+                print "Cannot import: %s" % url_path
+                print "Skipping app: %s" % app
             else:
                 urls.extend(getattr(url_module, 'URLS', []))
         return urls
