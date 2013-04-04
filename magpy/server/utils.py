7c905595ec0e4acbcd9cd22eabe9eafb78ed1420
zeth <theology@gmail.com>
2013-04-04 12:56:46 +0100

    Update utils.py

--- magpy/server/utils.py
+++ magpy/server/utils.py
@@ -1,4 +1,6 @@
 """Common simple utils."""
+import os
+from pkgutil import get_loader
 import json
 from bson import json_util
 
@@ -16,3 +18,8 @@ def instance_list_to_dict(list_of_instances):
     """Convert instance list to dict by id."""
     return {instance['_id']: instance for \
                 instance in list_of_instances}
+
+def get_mag_path():
+    """Return the file path of magpy."""
+    loader = get_loader('magpy')
+    return os.path.abspath(os.path.split(loader.get_filename())[0])
