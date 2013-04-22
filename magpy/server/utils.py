d3b1a77b467882bffbe7f5c26ae34f13b394caff
zeth <theology@gmail.com>
2013-04-22 16:14:30 +0100

    PEP8 on utils.py

--- magpy/server/utils.py
+++ magpy/server/utils.py
@@ -6,6 +6,7 @@ from bson import json_util
 import base64
 import uuid
 
+
 def dejsonify(value):
     """Converts from JSON when it is JSON, otherwise return it."""
     if value.startswith('JSON:'):
@@ -17,13 +18,15 @@ def dejsonify(value):
 
 def instance_list_to_dict(list_of_instances):
     """Convert instance list to dict by id."""
-    return {instance['_id']: instance for \
-                instance in list_of_instances}
+    return {instance['_id']: instance for
+            instance in list_of_instances}
 
-def get_mag_path(app = 'magpy'):
+
+def get_mag_path(app='magpy'):
     """Return the file path of magpy."""
     loader = get_loader(app)
     return os.path.abspath(os.path.split(loader.get_filename())[0])
+
 
 def make_cookie_secret():
     """Make a cookie secret."""
